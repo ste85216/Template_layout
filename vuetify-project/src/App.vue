669e557da1f8bb983e4ce191ebb8e14a4973db89
Steven <ste85213@gmail.com>
2024-06-24 16:14:56 +0800

    01

---
+++ vuetify-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-container class="d-flex align-center">
+        <v-app-bar-title>番茄鐘</v-app-bar-title>
+        <v-btn prepend-icon="mdi-home" to="/">首頁</v-btn>
+        <v-btn prepend-icon="mdi-format-list-bulleted" to="/list">事項</v-btn>
+        <v-btn prepend-icon="mdi-cog" to="/settings">設定</v-btn>
+      </v-container>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script setup>
+//
+</script>
