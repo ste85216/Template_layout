93e6a765baeead49c8620a13c4e1656bc3b99897
Steven <ste85213@gmail.com>
2024-06-27 16:43:36 +0800

    20240627

--- vuetify-project/src/App.vue
+++ vuetify-project/src/App.vue
@@ -1,16 +1,69 @@
 <template>
   <v-app>
-    <v-app-bar>
+    <v-app-bar class="bg-white custom-toolbar">
       <v-container class="d-flex align-center">
-        <v-app-bar-title>番茄鐘</v-app-bar-title>
-        <v-btn prepend-icon="mdi-home" to="/">首頁</v-btn>
-        <!-- <v-btn prepend-icon="mdi-format-list-bulleted" to="/list">事項</v-btn> -->
-        <!-- <v-btn prepend-icon="mdi-cog" to="/settings">設定</v-btn> -->
+        <!-- 左侧 LOGO -->
+        <v-img src="./assets/logo-volleycup.png" width="205px" class="logo" />
+
+        <!-- 右侧导航按钮（lg 及以上） -->
+        <v-row class="d-none d-md-flex" justify="end">
+          <v-btn>Home</v-btn>
+          <v-btn>About Us</v-btn>
+          <v-btn>Trainings</v-btn>
+          <v-btn>Packages</v-btn>
+          <v-btn>Blog</v-btn>
+          <v-btn>Contact</v-btn>
+          <v-btn>Events</v-btn>
+        </v-row>
+
+        <!-- 右侧汉堡菜单（lg 以下） -->
+        <v-btn icon class="d-flex d-md-none" @click="drawer = true">
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
       </v-container>
+
+      <!-- 左侧滑出菜单 -->
+      <v-navigation-drawer v-model="drawer" temporary absolute left>
+        <v-list>
+          <v-list-item v-for="page in pages" :key="page" @click="drawer = false">
+            <v-list-item-title>{{ page }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
     </v-app-bar>
+
+    <v-main>
+      <swiper></swiper>
+    </v-main>
   </v-app>
 </template>
 
 <script setup>
-//
+import { ref } from 'vue';
+import swiper from './components/swiper.vue';
+
+const drawer = ref(false);
+
 </script>
+
+<style lang="scss" scoped>
+.swiper {
+  margin-top: 41px;
+}
+
+.custom-toolbar,.v-toolbar__content {
+  height: 120px !important;
+}
+
+.bg-white {
+  background-color: white;
+}
+
+.logo {
+  max-height: 64px;
+}
+
+.v-application {
+  background: #fff;
+}
+</style>
---
+++ vuetify-project/src/components/swiper.vue
@@ -0,0 +1,87 @@
+<template>
+  <swiper 
+  :navigation="{
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }" 
+  :pagination="{
+        el: '.swiper-pagination',
+        clickable: true,
+      }"
+  :autoplay="{
+    delay: 2500,
+    disableOnInteraction: false
+  }" 
+  :loop="true" 
+  :modules="modules" 
+  class="mySwiper">
+    <swiper-slide><img src="https://picsum.photos/1920/700/?random=1"></swiper-slide>
+    <swiper-slide><img src="https://picsum.photos/1920/700/?random=2"></swiper-slide>
+    <swiper-slide><img src="https://picsum.photos/1920/700/?random=3"></swiper-slide>
+    <swiper-slide><img src="https://picsum.photos/1920/700/?random=4"></swiper-slide>
+    <swiper-slide><img src="https://picsum.photos/1920/700/?random=5"></swiper-slide>
+    <swiper-slide><img src="https://picsum.photos/1920/700/?random=6"></swiper-slide>
+    <swiper-slide><img src="https://picsum.photos/1920/700/?random=7"></swiper-slide>
+    <swiper-slide><img src="https://picsum.photos/1920/700/?random=8"></swiper-slide>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-pagination"></div>
+  </swiper>
+</template>
+<script>
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  // Import Swiper styles
+  import 'swiper/css/bundle';
+
+  import 'swiper/css/navigation';
+  import 'swiper/css/pagination';
+
+
+  // import required modules
+  import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      return {
+        modules: [Autoplay, Navigation, Pagination],
+      };
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .swiper-button-next,
+  .swiper-button-prev {
+    width: 60px;
+    height: 60px;
+    color: white;
+    background-color: #256fbb;
+    border-radius: 50%;
+    transition: 0.8s;
+  }
+  .swiper-button-next::after,
+  .swiper-button-prev::after {
+  font-size: 18px; /* 縮小箭頭大小 */
+  font-weight: 900; 
+  }
+  .swiper-button-prev::after {
+    position: absolute;
+    left: 23px;
+  }
+  .swiper-button-next:hover,
+  .swiper-button-prev:hover {
+    background: rgb(223, 203, 22);
+  }
+  .swiper-pagination-bullet {
+  width: 20px; /* 改變點點的寬度 */
+  height:20px; /* 改變點點的高度 */
+  background-color: #000; /* 改變點點的背景顏色 */
+  opacity: 1; /* 讓點點不透明 */
+}
+</style>
